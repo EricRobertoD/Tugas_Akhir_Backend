60c042c9868b51749fca462e905315558a99f448
Lepidophylla <ericroberto2322@gmail.com>
2024-05-22 05:13:55 +0700

    test

--- docker-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Run Laravel Artisan commands
-php artisan migrate --force
-php artisan passport:install --force
-php artisan api:install --force
-
-# Execute the CMD provided by the user
-exec "$@"
