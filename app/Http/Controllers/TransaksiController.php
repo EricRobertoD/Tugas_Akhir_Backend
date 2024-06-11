d349284d606ced4763556ac1934723befa5011bb
Lepidophylla <ericroberto2322@gmail.com>
2024-06-11 17:49:38 +0700

    fix transaksi

--- app/Http/Controllers/TransaksiController.php
+++ app/Http/Controllers/TransaksiController.php
@@ -37,31 +37,31 @@ class TransaksiController extends Controller
             'ids' => 'required|array',
             'ids.*' => 'exists:transaksi,id_transaksi',
         ]);
-    
+
         if ($validator->fails()) {
             return response()->json([
                 'message' => 'Validation failed',
                 'errors' => $validator->errors(),
             ], 400);
         }
-    
+
         $ids = $request->input('ids');
         $totalHarga = Transaksi::whereIn('id_transaksi', $ids)->sum('total_harga');
-    
+
         $user = auth()->user();
-        
+
         if ($user->saldo < $totalHarga) {
             return response()->json([
                 'message' => 'Saldo anda kurang.',
             ], 400);
         }
-    
+
         DB::beginTransaction();
-    
+
         try {
             $user->saldo -= $totalHarga;
             $user->save();
-    
+
             $saldo = new Saldo();
             $saldo->id_pengguna = $user->id_pengguna;
             $saldo->total = $totalHarga;
@@ -69,57 +69,57 @@ class TransaksiController extends Controller
             $saldo->tanggal = Carbon::today()->format('Y-m-d');
             $saldo->status = 'berhasil';
             $saldo->save();
-    
+
             $currentDate = Carbon::today();
             $year = $currentDate->year;
             $month = str_pad($currentDate->month, 2, '0', STR_PAD_LEFT);
             $day = str_pad($currentDate->day, 2, '0', STR_PAD_LEFT);
-    
+
             $lastTransaksi = Transaksi::whereDate('created_at', $currentDate)
+                ->where('invoice', 'like', "R-{$year}{$month}{$day}%")
                 ->orderBy('created_at', 'desc')
                 ->first();
-    
+
             if ($lastTransaksi) {
                 $lastInvoiceNumber = intval(substr($lastTransaksi->invoice, -3));
             } else {
                 $lastInvoiceNumber = 0;
             }
-    
-    
+
+
             foreach ($ids as $id) {
                 $transaksi = Transaksi::find($id);
-    
+
                 if (!$transaksi) {
                     throw new \Exception('Transaksi not found.');
                 }
-    
+
                 $lastInvoiceNumber++;
                 $newInvoiceNumber = str_pad($lastInvoiceNumber, 3, '0', STR_PAD_LEFT);
                 $invoice = "R-{$year}{$month}{$day}{$newInvoiceNumber}";
-    
+
                 $transaksi->status_transaksi = 'Sudah Bayar';
                 $transaksi->tanggal_pemesanan = Carbon::today()->format('Y-m-d');
                 $transaksi->invoice = $invoice;
                 $transaksi->save();
-    
+
                 $detailTransaksis = $transaksi->detailTransaksi;
                 foreach ($detailTransaksis as $detailTransaksi) {
                     $detailTransaksi->status_penyedia_jasa = 'Sedang Menghubungkan';
                     $detailTransaksi->save();
                 }
             }
-    
+
             DB::commit();
-    
+
             return response()->json([
                 'status' => 'success',
                 'message' => 'Transaksi updated successfully',
                 'data' => Transaksi::whereIn('id_transaksi', $ids)->get(),
             ], 200);
-    
         } catch (\Exception $e) {
             DB::rollBack();
-    
+
             return response()->json([
                 'message' => 'Update failed.',
                 'error' => $e->getMessage(),
