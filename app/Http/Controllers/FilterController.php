f8cd344253820b95f94f74543936c0ed0cd2bcd5
Lepidophylla <ericroberto2322@gmail.com>
2024-06-23 14:40:16 +0700

    fix filter

--- app/Http/Controllers/FilterController.php
+++ app/Http/Controllers/FilterController.php
@@ -72,7 +72,7 @@ class FilterController extends Controller
                     ->where('harga_paket', '<=', $endBudget);
             })
             ->where(function ($query) use ($date) {
-                $query->where('minimal_persiapan', '<=', Carbon::now()->diffInDays($date));
+                $query->where('minimal_persiapan', '<=', Carbon::now()->startOfDay()->diffInDays($date->startOfDay()));
             });
         
         if ($provinsiPenyedia !== 'Semua') {
